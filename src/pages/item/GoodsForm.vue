127eec78fb5513163f10713935ea880d520f8b49
yaorange2019 <yaorange2019@sina.com>
2019-05-29 18:24:16 +0800

    day8-pre

--- src/pages/item/GoodsForm.vue
+++ src/pages/item/GoodsForm.vue
@@ -258,7 +258,7 @@
         this.goods.spuDetail.specTemplate = JSON.stringify(obj);
 
         // 对全局规格参数进行深拷贝
-        const specs = [];
+        let specs = Object.deepCopy(this.specifications);
         Object.deepCopy(this.specifications, specs);
         specs.forEach(({params}) => {
           params.forEach(p => {
